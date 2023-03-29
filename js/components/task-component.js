2ab6ed3bd9af20ccd07ef0fb7d2d3ead30fa58c5
Gulmira <38330620+Gulmira817@users.noreply.github.com>
2023-03-30 02:29:37 +0400

    correct mistake css input none

--- js/components/task-component.js
+++ js/components/task-component.js
@@ -7,14 +7,14 @@ export default class TaskComponent {
 
     _getTemplate() {
         return (
-            `<div class="taskboard__item task  task--${this._task.status}" data-id="${this._task.id}">
-        <div class="task__body">
-          <p class="task--view">${this._task.title}</p>
-          <input type="text" class="task--input" />
-        </div>
-        <button aria-label="Изменить" class="task__edit" type="button"></button>
-      </div>`
-        );
+            `<div class="taskboard__item task task--${this._task.status}" data-id="${this._task.id}">
+              <div class="task__body">
+                <p class="task__view">${this._task.title}</p>
+                <input class="task__input" type="text" value="Название первой задачи">
+              </div>
+              <button class="task__edit" type="button" aria-label="Изменить"></button>
+            </div>`
+        )
     }
 
     getElement() {
